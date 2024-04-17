b6ea8780c0503ef8e438036e3e71461a3ad71f4d
betterpark <p980103@gmail.com>
2024-04-17 14:22:57 +0900

    02.intro 페이지 완료

--- src/js/intro.js
+++ src/js/intro.js
@@ -1,3 +1,23 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
 export function intro(){
-    
+    gsap.registerPlugin(ScrollTrigger)
+    const frameConst=9
+    const offsetValue=550
+
+    gsap.to('.img',{
+        backgroundPosition:(-offsetValue*frameConst)+'px',
+        ease:'steps('+frameConst+')',
+        scrollTrigger:{
+            trigger:'#intro',
+            start:'top top',
+            end:'+='+(frameConst*offsetValue),
+            pin:true,
+            scrub:true
+
+        }
+
+    })
+   
 }
